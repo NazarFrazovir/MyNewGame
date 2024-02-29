d3198b687e07b797121897e3379d16f74d3caa23
nazarfrazovir <nazarfrazovir@gmail.com>
2024-02-29 23:44:17 +0200

    Add operator= in .cpp file

--- MyGame/SwordMan.cpp
+++ MyGame/SwordMan.cpp
@@ -17,7 +17,13 @@ SwordMan::SwordMan(int new_sword_damage, int new_precision,int new_mobility)
 : Player("Unknown",0,0,0), sword_damage(new_sword_damage),
 precision(new_precision),mobility(new_mobility){}
 
-
+SwordMan &SwordMan::operator=(const SwordMan &other) {
+    if(this != &other){
+        Player::operator=(other);
+        sword_damage = other.sword_damage;
+    }
+    return *this;
+}
 
 SwordMan::~SwordMan() {}
 
@@ -27,6 +33,6 @@ void SwordMan::useSpell(const Spell& spell) {
     mobility += spell.getMobilityBonus();
 }
 
-void SwordMan::setSwordDamage(int new_sworddamage) {sword_damage=new_sworddamage;}
+void SwordMan::setSwordDamage(int new_sword_damage) {sword_damage=new_sword_damage;}
 void SwordMan::setMobility(int new_mobility) {mobility=new_mobility;}
 void SwordMan::setPrecision(int new_precision) {precision=new_precision;}
