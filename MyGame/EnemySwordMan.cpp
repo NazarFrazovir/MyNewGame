1f71a5919dd75c53d3e26a1cea08529a021fa21b
nazarfrazovir <nazarfrazovir@gmail.com>
2024-02-29 23:31:51 +0200

    Add derived class(EnemySwordMan) for Base class(Enemy), add move constructor,constructor,destructor,more methods

---
+++ MyGame/EnemySwordMan.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Nazar Frazovir on 29.02.2024.
+//
+#include "Enemy.h"
+#include <iostream>
+#include "EnemySwordMan.h"
+
+EnemySwordMan::EnemySwordMan():Enemy() {
+    enemy_sword_damage= 10 ;
+    enemy_precision = 5;
+    enemy_mobility = 5;
+}
+EnemySwordMan::EnemySwordMan(int new_enemy_sword_damage, int new_enemy_precision, int new_enemy_mobility)
+: Enemy("Unknown",0,0),enemy_sword_damage(new_enemy_sword_damage),
+enemy_precision(new_enemy_precision),enemy_mobility(new_enemy_mobility){}
+
+EnemySwordMan::EnemySwordMan(EnemySwordMan &&other) noexcept
+: Enemy(std::move(other)),
+enemy_sword_damage(other.enemy_sword_damage),enemy_mobility(other.enemy_mobility),enemy_precision(other.enemy_precision)
+{
+    other.enemy_sword_damage = 0;
+    other.enemy_precision = 0;
+    other.enemy_mobility = 0;
+}               // move constructor inheritance
+
+//EnemySwordMan::EnemySwordMan(const EnemySwordMan &other)
+//: Enemy("Unknown",0,0),enemy_sword_damage(other.enemy_sword_damage),          copy constructor for example
+//enemy_precision(other.enemy_precision),enemy_mobility(other.enemy_mobility){}
+EnemySwordMan::~EnemySwordMan() {}
+
+void EnemySwordMan::enemy_setSwordDamage(int new_enemy_sword_damage) {
+    this->enemy_sword_damage = new_enemy_sword_damage;
+}
+void EnemySwordMan::enemy_setMobility(int new_enemy_mobility) {
+    this->enemy_mobility = new_enemy_mobility;
+}
+void EnemySwordMan::enemy_setPrecision(int new_enemy_precision) {
+    this->enemy_precision = new_enemy_precision;
+}
+
+
+
+
+
---
+++ MyGame/EnemySwordMan.h
@@ -0,0 +1,29 @@
+//
+// Created by Nazar Frazovir on 29.02.2024.
+//
+
+#ifndef MYGAME_ENEMYSWORDMAN_H
+#define MYGAME_ENEMYSWORDMAN_H
+#include "Enemy.h"
+#include <iostream>
+
+class EnemySwordMan:public Enemy{
+private:
+    int enemy_sword_damage;
+    int enemy_precision;
+    int enemy_mobility;
+public:
+    EnemySwordMan();
+    EnemySwordMan(int new_enemy_sword_damage,int new_enemy_precision,int new_enemy_mobility);
+    EnemySwordMan(EnemySwordMan&& other) noexcept;
+//    EnemySwordMan(const EnemySwordMan& other);        copy constructor for example
+    ~EnemySwordMan();
+
+    void enemy_setSwordDamage(int new_enemy_sword_damage);
+    void enemy_setPrecision(int new_enemy_precision);
+    void enemy_setMobility(int new_enemy_mobility);
+
+
+};
+
+#endif //MYGAME_ENEMYSWORDMAN_H
