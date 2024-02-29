889f6d0c8ed4737a243fac7e852f71fdd7745d78
nazarfrazovir <nazarfrazovir@gmail.com>
2024-02-29 23:23:17 +0200

    Add destructor

--- MyGame/Spell.cpp
+++ MyGame/Spell.cpp
@@ -10,7 +10,7 @@ Spell::Spell()
 :spell_for_damage(0),spell_for_mobility(0),spell_for_precision(0){}
 Spell::Spell(int spell_for_damage, int spell_for_mobility, int spell_for_precision)
 :spell_for_damage(spell_for_damage),spell_for_mobility(spell_for_mobility),spell_for_precision(spell_for_precision){}
-
+Spell::~Spell() {}
 int Spell::getDamageBonus() const {
     return spell_for_damage;
 }
