38d73d5a4592f225bd72f6c7f052b83260516f42
nazarfrazovir <nazarfrazovir@gmail.com>
2024-03-06 11:54:01 +0200

    In this file I checked if the project works

--- MyGame/main.cpp
+++ MyGame/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "Player.h"
+#include "Enemy.h"
+#include "Game.h"
+#include "SwordMan.h"
+#include "Spell.h"
 
 int main() {
+    SwordMan Swordman1(10,100,100);
+    Spell spell1(10,10,10);
+    Swordman1.useSpell(spell1);         // Використання зілля
+    Swordman1.setName("Nazar");
+    Swordman1.setHealth(100);
+    Swordman1.stat();
 
     return 0;
 }
