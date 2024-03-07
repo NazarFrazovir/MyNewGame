f31db401107fdc41309fe0ffbaa298f7034153ad
nazarfrazovir <nazarfrazovir@gmail.com>
2024-03-07 22:43:16 +0200

    Add Static method binding, for display

--- MyGame/Player.cpp
+++ MyGame/Player.cpp
@@ -4,24 +4,28 @@
 #include <iostream>
 #include "Player.h"
 
-int Player::health = 100; // initialization static
+
+
 Player::Player()
 //        :name{"Unknown"}, health{0}, armor{0}, xp{0}
         : Player{"Unknown",0,0,0}{}
 
 Player::Player(std::string new_name, int new_health)
 //        :name{new_name}, health{new_health}
-        :Player{new_name,new_health,0,0}{}
+        :Player{new_name,new_health > 0 ? new_health : 0,0,0}{}
 
 Player::Player(std::string new_name, int new_health, int new_armor)
-        :Player{new_name,new_health,new_armor,0}{}
+        :Player{new_name,new_health > 0 ? new_health : 0,new_armor > 0 ? new_armor : 0
+                ,0}{}
 
 Player::Player(std::string new_name, int new_health, int new_armor, int new_xp)
-        :name{new_name},armor{new_armor},xp{new_xp}{health=new_health;}
+        :name{new_name},armor{new_armor},xp{new_xp}{health=new_health > 0 ? new_health : 0;}
 
 Player& Player::operator=(const Player &other){
         if (this != &other) {
-            this->armor = other.armor;         // Assigment operator - він потрібний для того щоб спрацював оператор -()
+            this->armor = other.armor;
+            this->name = other.name;
+            this->health = other.health;
         }
         return *this;
     }
@@ -34,7 +38,12 @@ Player Player::operator-() {
 
 Player::~Player() {}
 
-void Player::Weapon(){
+
+void Player::setHealth(int new_health) {
+    health = new_health;                    // this Вказує на поточний обʼєкт класу Player
+}
+
+void Player::weapons(){
     std::string weapon_class;
     double weapon_class_point;
     double damage;
@@ -118,26 +127,28 @@ void Player::Weapon(){
 
 }
 
-void Player::SetHealth(int new_health) {
-    health = new_health;                    // this Вказує на поточний обʼєкт класу Player
-}
-bool Player::HealtBool() {
-    if(health <= 0){
+bool Player::healtBool() {
+    if(health<= 0){
         return false;
     }else{
         return true;
     }
 }
-void Player::SetName(std::string new_name){
+
+void Player::setName(std::string new_name){
     this->name = new_name;
 }
-void Player::SetArmor(int new_armor) {
+
+void Player::setArmor(int new_armor) {
     this->armor = new_armor;
 }
-void Player::Stat() const{
-    std::cout<<"Player name: "<<Player::name<<std::endl;
+
+void Player::display() const{
+    std::cout<<"Name: "<<Player::name<<std::endl;
     std::cout<<"Health: "<<Player::health<<std::endl;
     std::cout<<"Armor: "<<Player::armor<<std::endl;
     std::cout<<std::endl;
 }
+
+void Player::displayAll(const Player &other) {other.display();} // Static binding method
 
